ee4295fb6c384977421160f2d9a372f01b2531d7
keithm-xmos <35048036+keithm-xmos@users.noreply.github.com>
2022-11-11 12:41:20 -0500

    fixes for new xcore_math API

--- modules/experimental/dsp_filters/src/dsp_filters.c
+++ modules/experimental/dsp_filters/src/dsp_filters.c
@@ -6,12 +6,12 @@
 
 #define EXP -31
 
-void apply_fir(xs3_filter_fir_s32_t * filter, int32_t DWORD_ALIGNED data[FILTER_FRAME_LENGTH]){
+void apply_fir(filter_fir_s32_t * filter, int32_t DWORD_ALIGNED data[FILTER_FRAME_LENGTH]){
 
     bfp_s32_t data_bfp;
 
     for(int v = 0; v < FILTER_FRAME_LENGTH; v++){
-        data[v] = xs3_filter_fir_s32(filter, data[v]);
+        data[v] = filter_fir_s32(filter, data[v]);
     }
     bfp_s32_init(&data_bfp, data, EXP, FILTER_FRAME_LENGTH, 1);
     // normalising the exponent
@@ -20,7 +20,7 @@ void apply_fir(xs3_filter_fir_s32_t * filter, int32_t DWORD_ALIGNED data[FILTER_
     memset(filter->state, 0, FIR_NUM_TAPS * sizeof(int32_t));
 }
 
-void apply_biquad(xs3_biquad_filter_s32_t * filter, int32_t DWORD_ALIGNED data[FILTER_FRAME_LENGTH]){
+void apply_biquad(filter_biquad_s32_t * filter, int32_t DWORD_ALIGNED data[FILTER_FRAME_LENGTH]){
 
     bfp_s32_t bfp;
     bfp_s32_init(&bfp, data, EXP, FILTER_FRAME_LENGTH, 1);
@@ -29,7 +29,7 @@ void apply_biquad(xs3_biquad_filter_s32_t * filter, int32_t DWORD_ALIGNED data[F
     bfp_s32_use_exponent(&bfp, BIQUAD_FILT_EXP);
 
     for(int v = 0; v < FILTER_FRAME_LENGTH; v++){
-        data[v] = xs3_filter_biquad_s32(filter, data[v]);
+        data[v] = filter_biquad_s32(filter, data[v]);
     }
     // using bfp because it has a saturation logic
     bfp_s32_use_exponent(&bfp, EXP);
