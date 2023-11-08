57ded6129ea41bce4ef4fb4afe3b0cd9e268a861
Chris Clarke <chrisc@xmos.com>
2023-11-08 11:49:10 +0000

    Moved tests out to a separate branch

--- modules/multitile_support/tests/tests.cmake
+++
@@ -1,4 +0,0 @@
-if(${CMAKE_SYSTEM_NAME} STREQUAL XCORE_XS3A)
-    #include(${CMAKE_CURRENT_LIST_DIR}/tile_map_defines/tests.cmake)
-    include(${CMAKE_CURRENT_LIST_DIR}/intertile_channels/CMakeLists.txt)
-endif()
